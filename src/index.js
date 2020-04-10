56883e063140155c1b4034d98897b83a1ee9b750
xenergyschool <xenergy_school@yahoo.co.uk>
2020-04-10 17:57:35 +0700

    Persiapan PWA
    Menambahkan ServiceWorker, Manifest dan icons

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import * as serviceWorker from "./serviceWorkers";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+
+serviceWorker.register();
